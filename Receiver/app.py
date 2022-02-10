65c2033ef7687cb0a998df3bf364e025f4024f80
digao666 <dgao12@my.bcit.ca>
2022-02-10 14:32:15 -0800

    add get to storage service

--- Receiver/app.py
+++ Receiver/app.py
@@ -1,12 +1,8 @@
 import connexion
 import requests
 from connexion import NoContent
-import datetime
-from datetime import datetime
 import json
-import os
 import yaml
-import logging
 import logging.config
 import uuid
 
@@ -25,40 +21,8 @@ with open('log_conf.yml', 'r') as f:
 logger = logging.getLogger('receiver')
 
 
-# def write_request(body):
-#     """ add request body to json """
-#     if os.path.isfile(EVENT_FILE):
-#         with open(EVENT_FILE, "r+") as file1:
-#             file_content = json.load(file1)
-#     else:
-#         file_content = []
-#     if 'fan_speed' in body:
-#         formatted_object = {
-#             "received_timestamp": str(datetime.now()),
-#             "request_data": str(
-#                 f"The GPU {body['ming_card_id']} of ming rig {body['ming_rig_id']}'s {body['fan_speed']['fan_position']}"
-#                 f" fan speed is {body['fan_speed']['fan_speed']}")
-#         }
-#     else:
-#         formatted_object = {
-#             "received_timestamp": str(datetime.now()),
-#             "request_data": str(f"The GPU {body['ming_card_id']} of ming rig {body['ming_rig_id']}'s core "
-#                                 f"temperature is {body['temperature']['core_temperature']}")
-#         }
-#     file_content.append(formatted_object)
-#
-#     if len(file_content) > MAX_EVENTS:
-#         file_content.pop(0)
-#     json_object = json.dumps(file_content, indent=4)
-#
-#     with open(EVENT_FILE, "w+") as file2:
-#         file2.write(json_object)
-#     return json_object
-
-
 def report_temperature(body):
     """ Receives a hardware temperature """
-    # write_request(body)
     trace_id = uuid.uuid1()
     body['trace_id'] = f'{trace_id}'
     payload = json.dumps(body)
@@ -77,7 +41,6 @@ def report_temperature(body):
 
 def report_fan_speed(body):
     """ Receives a fan speed """
-    # write_request(body)
     trace_id = uuid.uuid1()
     body['trace_id'] = f'{trace_id}'
     payload = json.dumps(body)
--- Storage/app.py
+++ Storage/app.py
@@ -8,10 +8,11 @@ from temperature import Temperature
 import yaml
 import logging
 import logging.config
+import datetime
+from datetime import datetime
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
-
 
 with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
@@ -19,14 +20,13 @@ with open('log_conf.yml', 'r') as f:
 
 logger = logging.getLogger('storage')
 
-
 user = app_config['datastore']['user']
 password = app_config['datastore']['password']
 port = app_config['datastore']['port']
 hostname = app_config['datastore']['hostname']
 db = app_config['datastore']['db']
 
-DB_ENGINE = create_engine (f'mysql+pymysql://{user}:{password}@{hostname}:{port}/{db}')
+DB_ENGINE = create_engine(f'mysql+pymysql://{user}:{password}@{hostname}:{port}/{db}')
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
@@ -65,6 +65,36 @@ def report_fan_speed(body):
     trace_id = body['trace_id']
     logger.debug(f'Stored event fan speed request with a trace id of {trace_id}')
     return NoContent, 201
+
+
+def get_temperature(timestamp):
+    """ Gets new temperature after the timestamp """
+    session = DB_SESSION()
+    timestamp_datetime = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    readings = session.query(Temperature).filter(Temperature.date_created >=
+                                                 timestamp_datetime)
+    results_list = []
+    for reading in readings:
+        results_list.append(reading.to_dict())
+    session.close()
+    logger.info("Query for Temperature readings after %s returns %d results" %
+                (timestamp, len(results_list)))
+    return results_list, 200
+
+
+def get_fan_speed(timestamp):
+    """ Gets new fan speed after the timestamp """
+    session = DB_SESSION()
+    timestamp_datetime = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    readings = session.query(Temperature).filter(FanSpeed.date_created >=
+                                                 timestamp_datetime)
+    results_list = []
+    for reading in readings:
+        results_list.append(reading.to_dict())
+    session.close()
+    logger.info("Query for fan speed readings after %s returns %d results" %
+                (timestamp, len(results_list)))
+    return results_list, 200
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
