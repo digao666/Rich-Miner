5f40c60639f5fabb2278434e7efee2afdd6a08b7
digao666 <dgao12@my.bcit.ca>
2022-02-16 23:39:36 -0800

    update database

--- Storage/create_tables_mysql.py
+++ Storage/create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="localhost", user="root", password="password", database="events")
+db_conn = mysql.connector.connect(host="digao3855.westus3.cloudapp.azure.com", user="user", password="password", database="events")
 db_cursor = db_conn.cursor()
 db_cursor.execute(''' 
     CREATE TABLE fan_speed (
--- Storage/drop_tables_mysql.py
+++ Storage/drop_tables_mysql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 
-db_conn = mysql.connector.connect(host="localhost", user="root", password="password", database="events")
+db_conn = mysql.connector.connect(host="digao3855.westus3.cloudapp.azure.com", user="user", password="password", database="events")
 db_cursor = db_conn.cursor()
 db_cursor.execute(''' DROP TABLE fan_speed, temperature ''')
 db_conn.commit()
